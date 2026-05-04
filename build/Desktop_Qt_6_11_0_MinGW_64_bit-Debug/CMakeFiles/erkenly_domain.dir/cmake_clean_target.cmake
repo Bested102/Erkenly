498add13939fc5e1bef242ec6529525ace724c00
El8azaly <mahmoud.elghazaly7002@gmail.com>
2026-05-04 23:02:48 +0300

    remove build

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/erkenly_domain.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "liberkenly_domain.a"
-)
--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/erkenly_domain.dir/erkenly_domain_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/erkenly_domain.dir/erkenly_domain_autogen/mocs_compilation.cpp.obj: \
- D:\AUC\CS2\ Lab\Erkenly\build\Desktop_Qt_6_11_0_MinGW_64_bit-Debug\erkenly_domain_autogen\mocs_compilation.cpp
